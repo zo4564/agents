0524c3db27dec46ee068981c0c382780cfc879da
zo4564 <zosiaa46@gmail.com>
2023-11-23 20:49:40 +0100

    poprawione spawnowanie wokół komórki centralnej i liczenie movers

--- Assets/Scripts/Heart.cs
+++ Assets/Scripts/Heart.cs
@@ -26,95 +26,8 @@ public class Heart : MonoBehaviour
     {
 
         Cells = new int[size, size];
-        
-        Random rnd = new Random();
-        int xHeart = rnd.Next(0, size);
-        int yHeart = rnd.Next(0, size);
-        Cells[xHeart, yHeart] = 99;
-
-        for (int x = 0; x < size; x++)
-        {
-            for (int y = 0; y < size; y++)
-            {
-                if (!(x == xHeart && y == yHeart))
-                {
-                    Cells[x, y] = rnd.Next(0, PossibleCells.Length + 1);
-                   
-                }
-            }
-        }
-
-
-
-
-        List<(int, int)> movers = new List<(int, int)>();
-        for (int x = 0; x < size; x++)
-        {
-            for (int y = 0; y < size; y++)
-            {
-                if (Cells[x, y] == 4)
-                {
-                    hasMover = true;
-                    (int xIndex, int yIndex) mover = (x, y);
-                    movers.Add(mover);
-                    Debug.Log(mover);
-                }
-            }
-        }
-        
-
-        int howManyMovers = movers.Count;
-        (int xIndex, int yIndex) centerCell = (xHeart, yHeart);
-        if (howManyMovers > 0)
-        {
-            centerCell = movers[rnd.Next(0, howManyMovers)];
-        }
-
-        for (int x = 0; x < size; x++)
-        {
-            for (int y = 0; y < size; y++)
-            {
-                if (Cells[x, y] == 4)
-                {
-                    (int xIndex, int yIndex) mover = (x, y);
-                    if (mover != centerCell)
-                    {
-                        Cells[x, y] = rnd.Next(1, PossibleCells.Length);
-                    }
-                }
-            }
-        }
-
-        if (hasMover)
-        {
-            centerObject = Instantiate(PossibleCells[3], Vector3.zero, Quaternion.identity);
-            originOffset = new Vector3((xHeart - centerCell.xIndex) * 10, 0, (yHeart - centerCell.yIndex) * 10);
-            transform.position = centerObject.transform.position + originOffset;
-            transform.SetParent(centerObject.transform, true);
-        }
-        else
-        {
-            centerObject = this.gameObject;
-            RandomSpawn(380, 210);
-        }
-
-       for (int x = 0; x < size; x++)
-        {
-            for (int y = 0; y < size; y++)
-            {
-                if (Cells[x, y] == 0) break;
-                int xIndex = centerCell.xIndex;
-                int yIndex = centerCell.yIndex;
-                if(!(x == xIndex && y == yIndex) && (!(x == xHeart && y == yHeart)))
-                {
-                    originOffset = new Vector3((x - xIndex) * 10, 0, (y - yIndex) * 10);
-                    Debug.Log(Cells[x, y] - 1);
-                    newCell = Instantiate(PossibleCells[Cells[x, y] - 1], Vector3.zero, Quaternion.identity);
-                    newCell.transform.position = centerObject.transform.position + originOffset;
-                    newCell.transform.SetParent(centerObject.transform, true);
-                }
-            }
-        }
+        GenerateCells();
+       
 
     }
 
@@ -133,5 +46,105 @@ public class Heart : MonoBehaviour
         float randomZ = UnityEngine.Random.Range(minPosition.z, maxPosition.z);
 
         transform.position = new Vector3(randomX, 0, randomZ);
+    }
+
+    void PlaceHeart()
+    {
+
+    }
+    void GenerateCells()
+    {
+        Random rnd = new Random();
+
+        //place heart
+        int xHeart = rnd.Next(0, size);
+        int yHeart = rnd.Next(0, size);
+        Cells[xHeart, yHeart] = 99;
+
+        //set heart as center (parent) cell
+        (int xIndex, int yIndex) centerCell = (xHeart, yHeart);
+
+        //generate rest of the cells
+        for (int x = 0; x < size; x++)
+        {
+            for (int y = 0; y < size; y++)
+            {
+                if (!(x == xHeart && y == yHeart))
+                {
+                    if (x >= xHeart - 1 && x <= xHeart + 1 && y >= yHeart - 1 && y <= yHeart + 1)
+                    {
+                        Cells[x, y] = rnd.Next(0, PossibleCells.Length + 1);
+                    }
+                    else Cells[x, y] = 0;
+
+
+
+                }
+            }
+        }
+
+        //count movers
+        List<(int, int)> movers = new List<(int, int)>();
+        for (int x = 0; x < size; x++)
+        {
+            for (int y = 0; y < size; y++)
+            {
+                if (Cells[x, y] == 4)
+                {
+                    hasMover = true;
+                    (int xIndex, int yIndex) mover = (x, y);
+                    movers.Add(mover);
+                    Debug.Log(mover);
+                }
+            }
+        }
+        int howManyMovers = movers.Count;
+
+        //set one mover to be center cell and remove other movers
+        if (howManyMovers > 0)
+        {
+            centerCell = movers[rnd.Next(0, howManyMovers)];
+            foreach ((int xIndex, int yIndex) mover in movers)
+            {
+                if (mover != centerCell)
+                {
+                    Cells[mover.xIndex, mover.yIndex] = rnd.Next(0, PossibleCells.Length);
+                    Debug.Log("bum");
+                }
+            }
+        }
+
+        //spawn center (parent) cell
+        if (hasMover)
+        {
+            centerObject = Instantiate(PossibleCells[3], Vector3.zero, Quaternion.identity);
+            originOffset = new Vector3((xHeart - centerCell.xIndex) * 10, 0, (yHeart - centerCell.yIndex) * 10);
+            transform.position = centerObject.transform.position + originOffset;
+            transform.SetParent(centerObject.transform, true);
+        }
+        else
+        {
+            centerObject = this.gameObject;
+            RandomSpawn(380, 210);
+        }
+
+        //spawn rest of cells
+       for (int x = 0; x < size; x++)
+        {
+            for (int y = 0; y < size; y++)
+            {
+                if (Cells[x, y] == 0) break;
+                int xIndex = centerCell.xIndex;
+                int yIndex = centerCell.yIndex;
+                if(!(x == xIndex && y == yIndex) && (!(x == xHeart && y == yHeart)))
+                {
+                    originOffset = new Vector3((x - xIndex) * 10, 0, (y - yIndex) * 10);
+                    Debug.Log(Cells[x, y] - 1);
+                    newCell = Instantiate(PossibleCells[Cells[x, y] - 1], Vector3.zero, Quaternion.identity);
+                    newCell.transform.position = centerObject.transform.position + originOffset;
+                    newCell.transform.SetParent(centerObject.transform, true);
+                }
+            }
+        }
     }
 }
